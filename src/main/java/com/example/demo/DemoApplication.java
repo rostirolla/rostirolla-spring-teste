bf4ed64027f31d8f392cab59577096ab499513c4
Carlos Souza <carlos.souza@teleconsistemas.com.br>
2020-03-11 16:50:28 -0300

    Primeiro commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,21 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Autowired
+	private ErrorAttributes errorAttributes;
+
+	@Bean
+	public AppErrorController appErrorController() {
+		return new AppErrorController(errorAttributes);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
