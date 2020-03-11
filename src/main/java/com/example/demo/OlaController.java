3b86c3af214b4d507f239ddb5d23aab8cfea7c8f
Carlos Souza <carlos.souza@teleconsistemas.com.br>
2020-03-11 18:11:04 -0300

    Primeiro commit

--- src/main/java/com/example/demo/OlaController.java
+++ src/main/java/com/example/demo/OlaController.java
@@ -2,11 +2,6 @@ package com.example.demo;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +13,8 @@ public class OlaController {
 	private final AtomicLong contador = new AtomicLong();
 
 	@GetMapping("/ola")
-	public ResponseBuilder ola(@RequestParam(value = "nome", defaultValue = "Mundo") String nome) {
-		return Response.ok(new Ola(contador.incrementAndGet(), String.format(template, nome)));
+	public Ola ola(@RequestParam(value = "nome", defaultValue = "Mundo") String nome) {
+		return new Ola(contador.incrementAndGet(), String.format(template, nome));
 	}
 	
 }
