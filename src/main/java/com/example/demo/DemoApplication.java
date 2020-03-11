33896949088901a251e051c8274b42ff77cbcfbe
Carlos Souza <carlos.souza@teleconsistemas.com.br>
2020-03-11 17:13:00 -0300

    Primeiro commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
