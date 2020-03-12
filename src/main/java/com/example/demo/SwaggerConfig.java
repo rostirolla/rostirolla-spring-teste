4f086c9df920898df6f38560a1cb3a49ed30fa48
Carlos Souza <carlos.souza@teleconsistemas.com.br>
2020-03-12 18:03:01 -0300

    Primeiro commit

--- src/main/java/com/example/demo/SwaggerConfig.java
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.example.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,18 @@ public class SwaggerConfig {
           .select()
           .apis(RequestHandlerSelectors.any())
           .paths(PathSelectors.any())
-          .build();
+          .build()
+          .apiInfo(apiInfo());
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Teste em Spring - Rostirolla")
+                .description("Demonstração de Teste de uma API com Spring, Gradle, JPA, Hibernate, Lombok banco H2 e documentação com Swagger. Foi utilizada a Spring Tool Suite (modificação do Eclipse) para desenvolvimento.")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("Carlos Henrique de Souza Rostirolla", "https://www.linkedin.com/in/carlosrostirolla/", "rostirolla@gmail.com"))
+                .build();
     }
 }
