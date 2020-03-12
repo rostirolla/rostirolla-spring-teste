0945cfe5b85d2141e302ad350bcb8ba45de5fc60
Carlos Souza <carlos.souza@teleconsistemas.com.br>
2020-03-12 16:22:48 -0300

    Primeiro commit

--- src/main/java/com/example/demo/PessoaController.java
+++ src/main/java/com/example/demo/PessoaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,25 +27,30 @@ public class PessoaController {
 	private PessoaRepository pessoaRepository;
 
 	@GetMapping
-	public List<Pessoa> listPessoas() {
-		return pessoaRepository.findAll();
+	public ResponseEntity<List<Pessoa>> listPessoas() {
+		return new ResponseEntity<List<Pessoa>>(pessoaRepository.findAll(), new HttpHeaders(), HttpStatus.FOUND);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Pessoa> getPessoa(@PathVariable("id") Long id) {
 		if (pessoaRepository.findById(id).isPresent())
-			return new ResponseEntity<Pessoa>(pessoaRepository.findById(id).get(), new HttpHeaders(), HttpStatus.OK);
+			return new ResponseEntity<Pessoa>(pessoaRepository.findById(id).get(), new HttpHeaders(), HttpStatus.FOUND);
 		return new ResponseEntity<Pessoa>(HttpStatus.NOT_FOUND);
 	}
 
 	@PostMapping
-	public Pessoa addPessoa(@RequestBody Pessoa pessoa) {
-		return pessoaRepository.save(pessoa);
+	public ResponseEntity<Pessoa> addPessoa(@RequestBody Pessoa pessoa) {
+		return new ResponseEntity<Pessoa>(pessoaRepository.save(pessoa), new HttpHeaders(), HttpStatus.CREATED);
 	}
 
 	@DeleteMapping
-	public String deletePessoa(@RequestParam(value = "id") Long id) {
+	public ResponseEntity<Pessoa> deletePessoa(@RequestParam(value = "id") Long id) {
 		pessoaRepository.deleteById(id);
-		return "Pessoa excluída com sucesso";
+		return new ResponseEntity<Pessoa>(HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Pessoa> putPessoa(@RequestBody Pessoa pessoa) {
+		return new ResponseEntity<Pessoa>(pessoaRepository.save(pessoa), new HttpHeaders(), HttpStatus.OK);
 	}
 }
